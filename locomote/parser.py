9c301c7c23083f1270d3241e55e8e003c72859df
Donatas Rasiukevicius <donatas@fly.io>
2024-08-06 23:33:37 +0200

    (yolo) simplify parser

--- locomote/parser.py
+++ locomote/parser.py
@@ -3,28 +3,20 @@ from difflib import ndiff
 
 from tiktoken import encoding_for_model
 
-from typing import AsyncIterator, Literal
+from typing import Literal
 
+GptEnc = encoding_for_model("gpt-4o")
+Speed = Literal["token", "newline"]
+
+def get_tokens(seq: str) -> list[str]:
+    tokens = GptEnc.encode(seq)
+    return [GptEnc.decode_single_token_bytes(x).decode() for x in tokens]
 
 @dataclass
-class Segment:
+class SeqMod:
     op: Literal["+", "-", " "]
     content: str
     start: int
-
-    def token_mods(self) -> list["Segment"]:
-        segments = []
-        enc = encoding_for_model("gpt-4o")
-        tokens = enc.encode(self.content)
-        decoded = [enc.decode_single_token_bytes(x).decode() for x in tokens]
-        cursor = self.start
-        for token in decoded:
-            if self.op == "+" or self.op == "-":
-                segments.append(Segment(self.op, token, cursor))
-            cursor += len(token)
-        if self.op == "-":
-            return sorted(segments, key=lambda x: x.start, reverse=True)
-        return segments
 
     def __call__(self, sequence: str) -> str:
         if self.op == "+":
@@ -35,111 +27,72 @@ class Segment:
             return sequence
 
     @classmethod
-    def from_diff_line(cls, line: str, cursor_index: int) -> "Segment":
+    def from_diff_line(cls, line: str, cursor_index: int) -> "SeqMod":
         return cls(line[0], line[2:], cursor_index)
 
     @classmethod
-    def from_diffs(cls, diffs: list[str], cursor: int) -> list["Segment"]:
-        segments = []
+    def from_diffs(cls, diffs: list[str], cursor: int) -> list["SeqMod"]:
+        mods = []
         for diff in diffs:
             if diff.startswith("?"):
                 continue
-            segment = cls.from_diff_line(diff, cursor)
-            if segment.op == " " or segment.op == "+":
-                cursor += len(segment.content)
-            segments.append(segment)
-        return segments
+            seq_mod = cls.from_diff_line(diff, cursor)
+            if seq_mod.op == " " or seq_mod.op == "+":
+                cursor += len(seq_mod.content)
+            mods.append(seq_mod)
+        return mods
 
     @classmethod
     def from_sequences(
         cls,
         seq_old: str,
         seq_new: str,
-        scope: Literal["doc", "line", "word"] = "doc",
+        speed: Speed = "token",
         cursor: int = 0,
-    ) -> list["Segment"]:
-        match scope:
-            case "doc":
+    ) -> list["SeqMod"]:
+        match speed:
+            case "newline":
                 diffs = ndiff(
-                    seq_old.splitlines(keepends=True), seq_new.splitlines(keepends=True)
+                    seq_old.splitlines(keepends=True), 
+                    seq_new.splitlines(keepends=True)
                 )
-                segments = cls.from_diffs(diffs, cursor)
-                segments = cls.resolve_segments(segments, "line")
-                return segments
-            case "newlines":
-                diffs = ndiff(
-                    seq_old.splitlines(keepends=True), seq_new.splitlines(keepends=True)
-                )
-                segments = cls.from_diffs(diffs, cursor)
-                return segments
-            case "line":
-                old_split = [x + " " for x in seq_old.split(" ")]
-                old_split[-1] = old_split[-1][:-1]
-                new_split = [x + " " for x in seq_new.split(" ")]
-                new_split[-1] = new_split[-1][:-1]
-                diffs = ndiff(old_split, new_split)
-                segments = cls.from_diffs(diffs, cursor)
-                segments = cls.resolve_segments(segments, "word")
-                return segments
-            case "word":
-                diffs = ndiff(seq_old, seq_new)
-                segments = cls.from_diffs(diffs, cursor)
-                return segments
-
-    @staticmethod
-    def resolve_segments(
-        segments: list["Segment"], scope: Literal["line", "word"]
-    ) -> list["Segment"]:
-        resolved = []
-        for segment in segments:
-            if segment.op == " ":
-                resolved.append(segment)
-            elif segment.op == "-":
-                if resolved and resolved[-1].op == "-":
-                    old = resolved.pop()
-                    resolved.append(
-                        Segment("-", old.content + segment.content, old.start)
-                    )
-                else:
-                    resolved.append(segment)
-            else:
-                if (
-                    resolved
-                    and resolved[-1].op == "-"
-                    and resolved[-1].start == segment.start
-                ):
-                    old = resolved.pop()
-                    seq_old, seq_new = old.content, segment.content
-                    resolved += Segment.from_sequences(
-                        seq_old, seq_new, scope, old.start
-                    )
-                elif resolved and resolved[-1].op == "+":
-                    old = resolved.pop()
-                    resolved.append(
-                        Segment("+", old.content + segment.content, old.start)
-                    )
-                else:
-                    resolved.append(segment)
-        return resolved
+                mods = cls.from_diffs(diffs, cursor)
+                return mods
+            case "token":
+                resolved = []
+                line_mods = cls.from_sequences(seq_old, seq_new, "newline", cursor)
+                for line_seg in line_mods:
+                    if line_seg.op == " " or not resolved:
+                        resolved.append(line_seg)
+                        continue
+                    if resolved[-1].op == "-" and line_seg.op == "+":
+                        prev = resolved.pop()
+                        seq_old, seq_new = prev.content, line_seg.content
+                        old_split, new_split = get_tokens(seq_old), get_tokens(seq_new)
+                        diffs = ndiff(old_split, new_split)
+                        resolved += cls.from_diffs(diffs, prev.start)
+                    else:
+                        resolved.append(line_seg)
+                return resolved
 
 
-async def generate_text_iter(
-    sequence_start: str, 
-    sequence_end: str,
-    speed: Literal["token", "newlines"] = "token"
-) -> AsyncIterator[str]:
-    yield sequence_start
-    if speed == "token":
-        segments = Segment.from_sequences(sequence_start, sequence_end)
-        for seg in segments:
-            for mod in seg.token_mods():
-                sequence_start = mod(sequence_start)
-                yield sequence_start
-    elif speed == "newlines":
-        segments = Segment.from_sequences(sequence_start, sequence_end, "newlines")
-        for seg in segments:
-            sequence_start = seg(sequence_start)
-            yield sequence_start
-    yield sequence_end
+# async def generate_text_iter(
+#     sequence_start: str, 
+#     sequence_end: str,
+#     speed: Speed = "token"
+# ) -> AsyncIterator[str]:
+#     yield sequence_start
+#     if speed == "token":
+#         segments = Segment.from_sequences(sequence_start, sequence_end)
+#         for seg in segments:
+#             for mod in seg.token_mods():
+#                 sequence_start = mod(sequence_start)
+#                 yield sequence_start
+#     elif speed == "newline":
+#         segments = Segment.from_sequences(sequence_start, sequence_end, "newline")
+#         for seg in segments:
+#             sequence_start = seg(sequence_start)
+#             yield sequence_start
+#     yield sequence_end
 
 
